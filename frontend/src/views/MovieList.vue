ea80a07a7017a4d89b104e7074639aef03cb7887
Kristi Rrapaj <74373101+kristirrapaj@users.noreply.github.com>
2025-04-10 11:53:54 +0200

    show fav movies

--- frontend/src/views/MovieList.vue
+++ frontend/src/views/MovieList.vue
@@ -1,32 +1,50 @@
 <template>
-  <div>
-    <div v-if="loading" class="loading-state">Loading movies...</div>
+<div>
+<div v-if="loading" class="loading-state">Loading movies...</div>
 
-    <div v-else-if="error" class="error-message">
-      ❌ {{ error }}
-      <button @click="fetchPopularMovies">Retry</button>
-    </div>
+<div v-else-if="error" class="error-message">
+  ❌ {{ error }}
+  <button @click="fetchPopularMovies">Retry</button>
+</div>
 
-    <div v-else class="movie-grid">
-      <MovieCard
-          v-for="movie in popularMovies"
-          :key="movie.id"
-          :movie="movie"
-      />
-    </div>
-  </div>
+<div v-else class="movie-grid">
+  <button @click="showFavorites" class="favorite-button">
+    {{ showOnlyFavorites ? 'Show All' : 'Show Favorites' }}
+  </button>
+  <MovieCard
+      v-for="movie in filteredMovies"
+      :key="movie.id"
+      :movie="movie"
+  />
+</div>
+</div>
 </template>
 
 <script setup>
 import { storeToRefs } from 'pinia'
 import { useMovieStore } from '@/stores/movieStore'
-import { onMounted } from 'vue'
+import {computed, onMounted, ref, watch} from 'vue'
 import MovieCard from '@/views/MovieCard.vue'
 
 const store = useMovieStore()
 const { popularMovies, loading, error } = storeToRefs(store)
-
 const { fetchPopularMovies } = store
+
+const showOnlyFavorites = ref(false)
+
+const filteredMovies = computed(() => {
+  if (showOnlyFavorites.value) {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || []
+    return popularMovies.value.filter(movie =>
+        favorites.some(fav => fav.id === movie.id)
+    )
+  }
+  return popularMovies.value
+})
+
+const showFavorites = () => {
+  showOnlyFavorites.value = !showOnlyFavorites.value
+}
 
 onMounted(() => {
   fetchPopularMovies()
